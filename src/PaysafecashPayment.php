b942e02efeaf16f38bc2b14c90afaff35f66539b
psc-fatos <70745532+psc-fatos@users.noreply.github.com>
2020-09-08 10:09:14 +0200

    Update PaysafecashPayment.php

--- src/PaysafecashPayment.php
+++ src/PaysafecashPayment.php
@@ -55,8 +55,8 @@ class PaysafecardCashController
         }
         curl_setopt($ch, CURLOPT_PORT, 443);
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
 
